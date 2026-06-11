883e812e5e37e1d3a1c77d5c3c5e4460b7065ae1
Karre Bhargav Reddy <24eg507f04@anurag.edu.in>
2026-06-11 21:10:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const display = document.getElementById("display");
 
-function appendToDisplay(input){
-    display.value += input;
+function appendToDisplay(input) {
+  display.value += input;
 }
-function clearDisplay(){
-    display.value="";
+
+function clearDisplay() {
+  display.value = "";
 }
-function calculate(){
-    try{
-        display.value = eval(display.value);
-    }
-    catch(error){
-        display.value = "Error";
-    }
+
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
 }
